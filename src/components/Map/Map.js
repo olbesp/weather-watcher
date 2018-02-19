f268b13eee24e931de8c25f0c8b49f69cdbcb14a
olbesp <olbesp365@gmail.com>
2018-02-19 16:24:05 +0200

    Rebuild map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+const apiKey = 'key=AIzaSyCNnMnCBQZ9HexzA0gigneirn4rWf-FeXU';
 
-const map = withScriptjs(withGoogleMap((props) =>
+const map = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?${apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `50vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+)((props) => (
   <GoogleMap
-    defaultZoom={12}
-    defaultCenter={{ lat: props.lat, lng: props.lng }}>
-    {props.isMarkerShown && <Marker position={{ lat: props.lat, lng: props.lng }} />}
+    defaultZoom={2}
+    defaultCenter={{ lat: 43, lng: 150 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 43, lng: 150 }} />}
   </GoogleMap>
-))
+));
 
 export default map;
+
