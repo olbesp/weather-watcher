2495189c12513356902ace391e92ba250e1964f7
olbesp <olbesp365@gmail.com>
2018-02-21 12:21:19 +0200

    Complete weatherBox state

--- src/containers/WeatherBox/WeatherBox.js
+++ src/containers/WeatherBox/WeatherBox.js
@@ -3,23 +3,26 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Aux from '../../hoc/Aux';
 import Background from '../../components/Background/Background';
+import DataBox from '../../components/DataBox/DataBox';
 
 class WeatherBox extends Component {
   state = {
-    weatherData: {},
-    currentWeather: ''
+    weatherData: {}
   }
 
   getWeather = () => {
     const url = `https://fcc-weather-api.glitch.me/api/current?lat=${this.props.coordinates.lat}&lon=${this.props.coordinates.lng}`;
     axios.get(url)
       .then(response => {
-        const weatherData = response.data;
-        const currentWeather = response.data.weather[0].main.toLowerCase();
-        this.setState({ 
-          weatherData,
-          currentWeather
-         });
+        const weatherData = {
+          weatherType: response.data.weather[0].main.toLowerCase(),
+          weatherDescription: response.data.weather[0].description,
+          temperature: { min: response.data.main.temp_min, max: response.data.main.temp_max },
+          wind: { deg: response.data.wind.deg, speed: response.data.wind.speed},
+          humidity: response.data.main.humidity,
+          pressure: response.data.main.pressure
+        };
+        this.setState({ weatherData });
         console.log(this.state);
       });
   }
@@ -33,7 +36,13 @@ class WeatherBox extends Component {
   render() {
     return (
       <Aux>
-        <Background time={this.props.time} currentWeather={this.state.currentWeather} />
+        <Background time={this.props.time} currentWeather={this.state.weatherData.weatherType} />
+        <DataBox>
+
+        </DataBox>
+        <DataBox>
+
+        </DataBox>
       </Aux>
     );
   }
